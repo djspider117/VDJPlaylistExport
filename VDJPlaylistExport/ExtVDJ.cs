082011646af1958dc655fb7880f9e1864026128a
Ionut Popovici <ionut.popovici@engageworks.com>
2023-11-14 22:49:12 +0200

    Initial commit

---
+++ VDJPlaylistExport/ExtVDJ.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace VDJPlaylistExport;
+
+public class ExtVDJ
+{
+    public int Filesize { get; set; }
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Remix { get; set; }
+    public double SongLength { get; set; }
+
+    public ExtVDJ(string xmlString)
+    {
+        xmlString = $"<root>{xmlString.Replace("&", "&amp;")}</root>";
+
+        XElement element = XElement.Parse(xmlString);
+        
+        Filesize = int.Parse(element.Element("filesize").Value);
+        Artist = WebUtility.HtmlDecode(element.Element("artist").Value);
+        Title = WebUtility.HtmlDecode(element.Element("title").Value);
+        Remix = WebUtility.HtmlDecode(element.Element("remix")?.Value ?? string.Empty);
+        SongLength = double.Parse(element.Element("songlength").Value);
+    }
+
+    public string GetSafeFilename()
+    {
+        string fname = ToString();
+
+        var invalidCharsPattern = $"[{Path.GetInvalidFileNameChars()}]";
+        var safeFileName = Regex.Replace(fname, invalidCharsPattern, "_");
+
+        return safeFileName;
+    }
+
+    public override string ToString()
+    {
+        string fname = null;
+        if (Title.ToLower().Contains(Remix.ToLower()))
+            fname = $"{Artist}-{Title}";
+        else
+            fname = $"{Artist}-{Title}({Remix})";
+
+        return fname;
+    }
+}
---
+++ VDJPlaylistExport/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace VDJPlaylistExport;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Minimum 2 arguments. First argument should be the path to the .m3u file (should be exported from Virtual DJ) and the second is a FOLDER path to where you want to export the playlist.");
+            return;
+        }
+
+        var m3uSrc = args[0];
+        var destFolder = args[1];
+
+        if (!File.Exists(m3uSrc))
+        {
+            Console.WriteLine($"Playlist file not found: {m3uSrc}");
+            return;
+        }
+
+        if (!Directory.Exists(destFolder))
+        {
+            Console.WriteLine($"Destination folder not found {destFolder}");
+            return;
+        }
+
+        var lines = await File.ReadAllLinesAsync(m3uSrc);
+
+        var existingFiles = Directory.GetFiles(destFolder);
+        if (existingFiles.Any())
+        {
+            Console.WriteLine("Destination folder already contains files. Do you want to clear the folder? (Y/N): ");
+            var resp = Console.ReadLine();
+            if (resp.ToLower() == "y")
+            {
+                Directory.Delete(destFolder, recursive: true);
+                Directory.CreateDirectory(destFolder);
+            }
+        }
+
+        Console.WriteLine("Starting copying files...");
+
+        for (int i = 0; i < lines.Length; i+=2)
+        {
+            var ext = lines[i];
+            var songPath = lines[i + 1];
+
+            var dir = Path.GetDirectoryName(songPath);
+            var extvdj = new ExtVDJ(ext.Replace("#EXTVDJ:", string.Empty));
+
+            var newFileName = $"{(i / 2):D3}. {extvdj.GetSafeFilename()}{Path.GetExtension(songPath)}";
+            var targetPath = Path.Combine(destFolder, newFileName);
+
+            Console.WriteLine(extvdj);
+            Console.WriteLine($"\t{songPath}");
+            Console.WriteLine($"\t{targetPath}");
+            Console.WriteLine();
+            File.Copy(songPath, targetPath, true);
+        }
+
+    }
+}
